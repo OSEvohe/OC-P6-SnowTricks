4d4ddd885642f8ebc60cb9334aff58e5d27c1afc
Sebastien <sebastien@o-pa.fr>
2020-10-01 16:00:49 +0200

    Switch to Sass

--- assets/app.js
+++ assets/app.js
@@ -6,8 +6,7 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/global.scss';
 
 // Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
 // import $ from 'jquery';
