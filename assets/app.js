53e5dc060489573f006dd8e89f51ac93de44f48d
Sebastien <sebastien@o-pa.fr>
2020-10-08 23:53:24 +0200

    Add, Edit, Delete trick

--- assets/app.js
+++ assets/app.js
@@ -1,32 +1,41 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
 import './styles/global.scss';
-
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
-
 const $ = require('jquery');
-// this "modifies" the jquery module: adding behavior to it
-// the bootstrap module doesn't export/return anything
 require('bootstrap');
 
 $(document).ready(function () {
     $('[data-toggle="popover"]').popover();
 
+    // Show or hide "See Medias" button
     let resizeTimer;
     showMediaList();
     $(window).resize(function () {
         clearTimeout(resizeTimer);
         resizeTimer = setTimeout(showMediaList, 100);
     });
+
+    // Display delete trick modal
+    $('#deleteTrickModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget)
+        var trickId = button.data('trickid')
+        var trickName = button.data('trickname')
+        var modal = $(this)
+        modal.find('.modal-title-trickName').text(trickName)
+        modal.find('.modal-body #trickId').val(trickId)
+    })
+
+    // Display delete media modal
+    $('#deleteMediaModal').on('show.bs.modal', function (event) {
+        var button = $(event.relatedTarget)
+        var mediaId = button.data('mediaid')
+        var mediaName = button.data('medianame')
+        var modal = $(this)
+        modal.find('.modal-title-mediaName').text(mediaName)
+        modal.find('.modal-body #mediaId').val(mediaId)
+    })
 });
 
+
+// collapse or reduce the media list
 function showMediaList() {
     let cssValue = $('#show-media-button').css('display');
     if (cssValue === 'none') {
