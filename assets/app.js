557653901e2664d44bea659c1adbdc6ff215e65b
Sebastien <sebastien@o-pa.fr>
2020-10-09 15:31:13 +0200

    Refactor for CodeClimate

--- assets/app.js
+++ assets/app.js
@@ -14,24 +14,9 @@ $(document).ready(function () {
         resizeTimer = setTimeout(showMediaList, 100);
     });
 
-    // Display delete trick modal
-    $('#deleteTrickModal').on('show.bs.modal', function (event) {
-        setModalFormValue(event,
-            {
-                trickid: {type: 'val', selector: '.trickId'},
-                trickname: {type: 'text', selector: '.trickName'}
-            },
-            $(this))
-    })
-
-    // Display delete media modal
-    $('#deleteMediaModal').on('show.bs.modal', function (event) {
-        setModalFormValue(event,
-            {
-                mediaid: {type: 'val', selector: '.mediaId'},
-                medianame: {type: 'text', selector: '.mediaName'}
-            },
-            $(this))
+    // Display delete modal
+    $('#deleteModal').on('show.bs.modal', function (event) {
+        setModalFormValues(event, $(event.relatedTarget).data(), $(this))
     })
 });
 
@@ -47,21 +32,23 @@ function showMediaList() {
 }
 
 /**
- * Set form value, allow to use the same modal for multiple item
+ * Set form values, allow to use the same modal for multiple item
  * event :          show modal event
  * modalValue :     Object of Objects following this structure 'dataname : {type: 'type', selector: 'css-selector'}
  * example          HTML : <button data-myid="1">My Button</button><input type='hidden' class='my-id' name='id' />
  *                  JS : modalValues = {myid : {type: 'val, selector: '.my-id}}
  * modal            The modal
  */
-function setModalFormValue(event, modalValue, modal) {
-    for (let key in modalValue) {
-        let item = modal.find(modalValue[key].selector)
-        let value = $(event.relatedTarget).data(key)
-        if (modalValue[key].type === 'val') {
-            item.val(value)
-        } else {
-            item.text(value)
+function setModalFormValues(event, modalValues, modal) {
+    for (let key in modalValues) {
+        if (modalValues.hasOwnProperty(key) && modalValues[key].selector !== undefined) {
+            let field = modalValues[key]
+            let item = modal.find(field.selector)
+            if (field.type === 'val') {
+                item.val(field.value)
+            } else {
+                item.text(field.value)
+            }
         }
     }
 }
