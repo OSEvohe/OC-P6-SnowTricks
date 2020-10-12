61316cbd8cb60263d6e7ae672c830baa86f4c3d6
Sebastien <sebastien@o-pa.fr>
2020-10-12 09:22:28 +0200

    User Entity

---
+++ src/Entity/Trick.php
@@ -0,0 +1,209 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrickRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TrickRepository::class)
+ */
+class Trick
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $slug;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=TrickGroup::class, inversedBy="tricks")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $trickgroup;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="tricks")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @ORM\OneToMany(targetEntity=UserTrick::class, mappedBy="trick")
+     */
+    private $userTricks;
+
+    /**
+     * @ORM\OneToMany(targetEntity=TrickMedia::class, mappedBy="trick")
+     */
+    private $trickMedia;
+
+    /**
+     * @ORM\OneToOne(targetEntity=TrickMedia::class, inversedBy="cover", cascade={"persist", "remove"})
+     */
+    private $cover;
+
+
+    public function __construct()
+    {
+        $this->userTricks = new ArrayCollection();
+        $this->trickMedia = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getTrickGroup(): ?TrickGroup
+    {
+        return $this->TrickGroup;
+    }
+
+    public function setTrickGroup(?TrickGroup $TrickGroup): self
+    {
+        $this->TrickGroup = $TrickGroup;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|UserTrick[]
+     */
+    public function getUserTricks(): Collection
+    {
+        return $this->userTricks;
+    }
+
+    public function addUserTrick(UserTrick $userTrick): self
+    {
+        if (!$this->userTricks->contains($userTrick)) {
+            $this->userTricks[] = $userTrick;
+            $userTrick->setTrick($this);
+        }
+
+        return $this;
+    }
+
+    public function removeUserTrick(UserTrick $userTrick): self
+    {
+        if ($this->userTricks->contains($userTrick)) {
+            $this->userTricks->removeElement($userTrick);
+            // set the owning side to null (unless already changed)
+            if ($userTrick->getTrick() === $this) {
+                $userTrick->setTrick(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|TrickMedia[]
+     */
+    public function getTrickMedia(): Collection
+    {
+        return $this->trickMedia;
+    }
+
+    public function addTrickMedium(TrickMedia $trickMedium): self
+    {
+        if (!$this->trickMedia->contains($trickMedium)) {
+            $this->trickMedia[] = $trickMedium;
+            $trickMedium->setTrick($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTrickMedium(TrickMedia $trickMedium): self
+    {
+        if ($this->trickMedia->contains($trickMedium)) {
+            $this->trickMedia->removeElement($trickMedium);
+            // set the owning side to null (unless already changed)
+            if ($trickMedium->getTrick() === $this) {
+                $trickMedium->setTrick(null);
+            }
+        }
+
+        return $this;
+    }
+
+    public function getCover(): ?TrickMedia
+    {
+        return $this->cover;
+    }
+
+    public function setCover(?TrickMedia $cover): self
+    {
+        $this->cover = $cover;
+
+        return $this;
+    }
+}
---
+++ src/Entity/TrickGroup.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TrickGroupRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TrickGroupRepository::class)
+ */
+class TrickGroup
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=40)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=60)
+     */
+    private $slug;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Trick::class, mappedBy="trickgroup")
+     */
+    private $tricks;
+
+    public function __construct()
+    {
+        $this->tricks = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Trick[]
+     */
+    public function getTricks(): Collection
+    {
+        return $this->tricks;
+    }
+
+    public function addTrick(Trick $trick): self
+    {
+        if (!$this->tricks->contains($trick)) {
+            $this->tricks[] = $trick;
+            $trick->setTrickGroup($this);
+        }
+
+        return $this;
+    }
+
+    public function removeTrick(Trick $trick): self
+    {
+        if ($this->tricks->contains($trick)) {
+            $this->tricks->removeElement($trick);
+            // set the owning side to null (unless already changed)
+            if ($trick->getTrickGroup() === $this) {
+                $trick->setTrickGroup(null);
+            }
+        }
+
+        return $this;
+    }
+}
