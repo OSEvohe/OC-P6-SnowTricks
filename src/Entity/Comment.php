82f021bb334b23a2097902a0fd81a3d93f202194
Sebastien <sebastien@o-pa.fr>
2020-10-15 12:23:40 +0200

    Add relation between Comment and User

--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -28,6 +28,12 @@ class Comment
      */
     private $trick;
 
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -53,6 +59,18 @@ class Comment
     public function setTrick(?Trick $trick): self
     {
         $this->trick = $trick;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
 
         return $this;
     }
