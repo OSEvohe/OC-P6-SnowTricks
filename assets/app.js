04b0dc7349d8d52d9230061b8839492bcbc2de3b
Sebastien <sebastien@o-pa.fr>
2020-10-20 21:43:24 +0200

    Add or remove items from collection

--- assets/app.js
+++ assets/app.js
@@ -24,69 +24,52 @@ $(document).ready(function () {
         }
     })
 
-    // FORMULAIRE AVEC NOMBRE VARIABLE DE TRICKMEDIA
-    var $container = $('div#trick_trickMediaPicture');
-    // On définit un compteur unique pour nommer les champs qu'on va ajouter dynamiquement
-    var index = $container.find('fieldset').length;
-    // On ajoute un nouveau champ à chaque clic sur le lien d'ajout.
-    $('#add_trickMedia').click(function (e) {
-        addTrickMedia($container);
+    // Display additional media form
+    let addMediaImagePrototype = $('div#trick_trickMediaPicture')
+    let addMediaVideoPrototype = $('div#trick_trickMediaVideo')
+    let addMediaContainer = $('div#additional_medias')
+    let index = {nb: 0}
 
-        e.preventDefault(); // évite qu'un # apparaisse dans l'URL
+    $('#add_trickMediaPicture').click(function (e) {
+        addTrickMedia(addMediaImagePrototype, addMediaContainer, index);
+        e.preventDefault();
         return false;
     });
 
-    // On ajoute un premier champ automatiquement s'il n'en existe pas déjà un (cas d'un nouveau trick).
-    if (index === 0) {
-        addTrickMedia($container);
-    } else {
-        // S'il existe déjà des medias, on ajoute un lien de suppression pour chacun d'entre eux.
-        $container.children('fieldset').each(function (index) {
-            $(this).children('legend').text('Media ' + (index + 1))
-            addDeleteLink($(this));
-        });
+    $('#add_trickMediaVideo').click(function (e) {
+        addTrickMedia(addMediaVideoPrototype, addMediaContainer, index);
+        e.preventDefault();
+        return false;
+    });
+
+    addMediaContainer.find('.trick-media-form').each(function() {
+        addDeleteLink($(this));
+    });
+
+    function addTrickMedia(prototype, container, index) {
+        index.nb++
+        console.log(index)
+        let template = prototype.attr('data-prototype')
+            .replace(/__name__label__/g, 'Media')
+            .replace(/__name__/g, index.nb);
+
+        let form = $(template);
+        addDeleteLink(form);
+        container.append(form);
     }
 
-    // La fonction qui ajoute un formulaire TrickMediaType
-    function addTrickMedia($container) {
-        // On définit un compteur unique pour nommer les champs qu'on va ajouter dynamiquement
-        var index = $container.find('fieldset').length;
-        // Dans le contenu de l'attribut « data-prototype », on remplace :
-        // - le texte "__name__label__" qu'il contient par le label du champ
-        // - le texte "__name__" qu'il contient par le numéro du champ
-        var template = $container.attr('data-prototype')
-            .replace(/__name__label__/g, 'Media ' + (index + 1))
-            .replace(/__name__/g, index)
-        ;
 
-        // On crée un objet jquery qui contient ce template
-        var $prototype = $(template);
+    function addDeleteLink(prototype) {
+        let deleteLink = $('<a href="#" class="btn btn-danger">Supprimer</a>');
+        prototype.append(deleteLink);
 
-        // On ajoute au prototype un lien pour pouvoir supprimer la catégorie
-        addDeleteLink($prototype);
-
-        // On ajoute le prototype modifié à la fin de la balise <div>
-        $container.append($prototype);
-    }
-
-// La fonction qui ajoute un lien de suppression d'une catégorie
-    function addDeleteLink($prototype) {
-        // Création du lien
-        var $deleteLink = $('<a href="#" class="btn btn-danger">Supprimer</a>');
-
-        // Ajout du lien
-        $prototype.append($deleteLink);
-
-        // Ajout du listener sur le clic du lien pour effectivement supprimer la catégorie
-        $deleteLink.click(function (e) {
-            $prototype.remove();
-            $container.children('fieldset').each(function (index) {
-                $(this).children('legend').text('Tarif n°' + (index + 1))
-            });
-            e.preventDefault(); // évite qu'un # apparaisse dans l'URL
+        deleteLink.click(function (e) {
+            prototype.remove();
+            e.preventDefault();
             return false;
         })
     }
+
 });
 
 
