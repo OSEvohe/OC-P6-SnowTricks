c86303036b7a42906310e4d60a1d2bb00db60cbe
Sebastien <sebastien@o-pa.fr>
2020-10-21 23:50:02 +0200

    Homepage with tricks from db

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Entity\Trick;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -21,7 +22,13 @@ class HomeController extends AbstractController
      */
     public function index(): Response
     {
-        return $this->render('index.html.twig');
+        $tricks = $this->getDoctrine()
+            ->getRepository(Trick::class)
+            ->findBy([], ['updatedAt' => 'DESC'], 15);
+
+        return $this->render('index.html.twig', [
+            'tricks' => $tricks
+        ]);
     }
 
 }
