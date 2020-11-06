b174e4c0d1ce569984bce2cb6ed08c00954bdf37
Sebastien <sebastien@o-pa.fr>
2020-11-06 12:09:43 +0100

    loadmore script for tricks list

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,9 +5,13 @@ namespace App\Controller;
 
 
 use App\Entity\Trick;
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
+use Symfony\Component\Serializer\SerializerInterface;
 
 /**
  * Class HomeController
@@ -18,17 +22,60 @@ class HomeController extends AbstractController
     /**
      * View Homepage
      *
-     * @Route("/", name="home")
+     * @Route ("/", name="home")
+     * @param TrickRepository $em
+     * @return Response
      */
-    public function index(): Response
+    public function index(TrickRepository $em): Response
     {
-        $tricks = $this->getDoctrine()
-            ->getRepository(Trick::class)
-            ->findBy([], ['updatedAt' => 'DESC'], 15);
-
+        $tricks = $em->findBy([], ['updatedAt' => 'DESC'],12);
         return $this->render('index.html.twig', [
-            'tricks' => $tricks
+            'tricks' => $tricks,
+            'countTotal' => $em->count([])
         ]);
     }
+
+
+    /**
+     * Fetch more trick in JSON response
+     *
+     * @Route ("/trick/load-more/{offset}/{limit}", name="load_more_tricks")
+     * @param TrickRepository $em
+     * @param SerializerInterface $serializer
+     * @param int $offset
+     * @param int $limit
+     * @return Response
+     */
+    public function loadMore(TrickRepository $em, SerializerInterface $serializer, int $offset = 0, int $limit = 15): Response
+    {
+        $tricks = $em->findBy([], ['updatedAt' => 'DESC'], $limit, $offset);
+
+        // Return an array with the slug, trick detail route and trick delete route
+        $slugCallback = function ($value, $object, string $attributeName, string $format = null, array $context = [])
+        {
+           return [
+               'slug' => $value,
+               'detail_route' => $this->generateUrl('trick_detail', ['slug' => $value]),
+               'edit_route' => $this->generateUrl('trick_edit', ['slug' => $value]),
+               'delete_route' => $this->generateUrl('trick_delete', ['slug' => $value]),
+               ];
+        };
+
+        return new JsonResponse(
+            $serializer->serialize($tricks, 'json', [
+                AbstractNormalizer::ATTRIBUTES => [
+                    'name',
+                    'slug',
+                    'cover' => ['content'],
+                ],
+                AbstractNormalizer::CALLBACKS => [
+                    'slug' => $slugCallback,
+                ],
+            ])
+            , JsonResponse::HTTP_OK, [], true
+        );
+    }
+
+
 
 }
