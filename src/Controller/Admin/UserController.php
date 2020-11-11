0c085514dd2e7f0393fddfe8e644cfb834ec6069
Sebastien <sebastien@o-pa.fr>
2020-11-11 16:16:37 +0100

    Mini Admin restricted to admin

---
+++ src/Controller/Admin/UserController.php
@@ -0,0 +1,118 @@
+<?php
+
+
+namespace App\Controller\Admin;
+
+
+use App\Entity\Comment;
+use App\Entity\Trick;
+use App\Entity\User;
+use App\Form\ProfileType;
+use App\Repository\CommentRepository;
+use App\Repository\UserRepository;
+use App\Service\ImageUploader;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/_sntrks_admin/user", name="app_admin_user_")
+ */
+class UserController extends AbstractController
+{
+
+    /**
+     * @Route("/", name="index")
+     *
+     * @param UserRepository $repository
+     * @return Response
+     */
+    public function index(UserRepository $repository): Response
+    {
+        return $this->render('admin/user/index.html.twig', [
+            'users' => $repository->findAll()
+        ]);
+    }
+
+
+    /**
+     * @Route ("/{id}/edit", name="update")
+     * @param Request $request
+     * @param User $user
+     * @param EntityManagerInterface $em
+     * @return Response
+     */
+    public function update(Request $request, User $user, EntityManagerInterface $em): Response
+    {
+        $form = $this->createForm(ProfileType::class);
+        $form->remove('photo');
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($form->getData());
+            $em->flush();
+
+            $this->addFlash('success', 'L\'utilisateur a été modifié');
+            return $this->redirectToRoute('app_admin_user_index');
+        }
+
+        return $this->render('admin/user/update.html.twig', [
+            'user' => $user,
+            'form' => $form->createView()
+        ]);
+    }
+
+
+    /**
+     * @Route ("/{id}/delete", name="delete")
+     * @param User $user
+     * @param EntityManagerInterface $em
+     * @return RedirectResponse
+     */
+    public function delete(User $user, EntityManagerInterface $em): RedirectResponse
+    {
+        if (false === array_search(USER::USER_ADMIN, $user->getRoles())) {
+            // transfer tricks ownership to the current logged admin user
+            foreach ($user->getTricks() as $trick){
+                $trick->setUser($this->getUser());
+            }
+
+
+            $em->remove($user);
+            $em->flush();
+            $this->addFlash('success', 'L\'utilisateur a été supprimé');
+        } else {
+            $this->addFlash('error', 'Un compte avec le role Admin ne peux être supprimé');
+        }
+
+        return $this->redirectToRoute('app_admin_user_index');
+    }
+
+    /**
+     * @Route ("/{id}/delete-photo", name="delete_photo")
+     *
+     * @param User $user
+     * @param EntityManagerInterface $em
+     * @param ImageUploader $imageUploader
+     * @param ParameterBagInterface $parameterBag
+     * @return RedirectResponse
+     */
+    public function deleteUserPhoto(User $user, EntityManagerInterface $em, ImageUploader $imageUploader, ParameterBagInterface $parameterBag): RedirectResponse
+    {
+        if ($user->getPhoto()) {
+            $imageUploader->setTargetDirectory($parameterBag->get('kernel.project_dir') . '/public/uploads/profiles');
+            $imageUploader->deleteFile($user->getPhoto());
+        }
+
+        $user->setPhoto(null);
+        $em->persist($user);
+        $em->flush();
+
+        return $this->redirectToRoute('app_admin_user_update', ['id' => $user->getId()]);
+    }
+}
--- src/Form/ProfileType.php
+++ src/Form/ProfileType.php
@@ -5,49 +5,90 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ButtonType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\File;
 use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ProfileType extends AbstractType
 {
+    private $security;
+
+
+    /**
+     * ProfileType constructor.
+     * @param Security $security
+     */
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('photo', FileType::class, [
-                'label' => 'Nouvelle photo du profil',
-                'required' => false,
-                'mapped' => false,
-                'constraints' => [
-                    new File([
-                        'maxSize' => '256k',
-                        'mimeTypes' => [
-                            'image/jpeg',
-                            'image/gif',
-                            'image/png'
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            $form = $event->getForm();
+            if ($this->security->isGranted(User::USER_VERIFIED)) {
+                $form
+                    ->add('photo', FileType::class, [
+                        'label' => 'Nouvelle photo du profil',
+                        'required' => false,
+                        'mapped' => false,
+                        'constraints' => [
+                            new File([
+                                'maxSize' => '256k',
+                                'mimeTypes' => [
+                                    'image/jpeg',
+                                    'image/gif',
+                                    'image/png'
+                                ],
+                                'mimeTypesMessage' => 'Type de fichier invalide, les formats acceptés sont JPG, GIF et PNG.',
+                            ]),
+                        ]
+                    ])
+                    ->add('displayName', TextType::class, [
+                        'attr' => ['class' => 'form-control'],
+                        'label' => 'Nouveau pseudo',
+                        'help_attr' => ['class' => 'small'],
+                        'help' => 'Laissez vide pour ne pas le modifier.',
+                        'required' => false,
+                        'constraints' => [
+                            new Length([
+                                'normalizer' => 'trim',
+                                'min' => 2, 'minMessage' => 'Le Pseudo doit contenir au minimum {{ limit }} caractères',
+                                'max' => 30, 'maxMessage' => 'Le Pseudo doit contenir au maximum {{ limit }} caractères',
+                                'allowEmptyString' => false
+                            ]),
                         ],
-                        'mimeTypesMessage' => 'Type de fichier invalide, les formats acceptés sont JPG, GIF et PNG.',
-                    ]),
-                ]
-            ])
-            ->add('displayName', TextType::class, [
-                'attr' => ['class' => 'form-control'],
-                'label' => 'Nouveau pseudo',
-                'help_attr' => ['class' => 'small'],
-                'help' => 'Laissez vide pour ne pas le modifier.',
-                'required' => false,
-                'constraints' => [
-                    new Length([
-                        'normalizer' => 'trim',
-                        'min' => 2, 'minMessage' => 'Le Pseudo doit contenir au minimum {{ limit }} caractères',
-                        'max' => 30, 'maxMessage' => 'Le Pseudo doit contenir au maximum {{ limit }} caractères',
-                        'allowEmptyString' => false
-                    ]),
-                ],
-            ]);
+                    ]);
+                if ($this->security->isGranted(User::USER_ADMIN)) {
+                    $form->add('email', EmailType::class, [
+                        'label' => 'Nouvelle adresse email',
+                        'help_attr' => ['class' => 'small'],
+                        'help' => 'Laissez vide pour ne pas la modifier.',
+                        'required' => false,
+                        'constraints' => [
+                            new Email(['message' => '"{{ value }}" n\'est pas une adresse e-mail valide.']),
+                        ]
+                    ]);
+                }
+                $form->add('save', SubmitType::class, [
+                    'label' => 'Enregistrer',
+                    'attr' => ['class' => 'btn btn-success'],
+                    'row_attr' => ['class' => 'd-inline-block float-right']
+                ]);
+            }
+        });
     }
 
     public function configureOptions(OptionsResolver $resolver)
