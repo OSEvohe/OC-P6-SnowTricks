b22d671967eac4949c653986d0dc0ae116253eb6
Sebastien <sebastien@o-pa.fr>
2020-11-13 23:17:24 +0100

    Test environment setup

--- config/bundles.php
+++ config/bundles.php
@@ -18,4 +18,5 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
--- src/DataFixtures/TricksFixture.php
+++ src/DataFixtures/TricksFixture.php
@@ -4,9 +4,12 @@
 namespace App\DataFixtures;
 
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\PasswordEncoderInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 /**
  * Sample data
@@ -17,12 +20,33 @@ use Doctrine\Persistence\ObjectManager;
  */
 class TricksFixture extends Fixture implements FixtureGroupInterface
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
 
     /**
      * @inheritDoc
      */
     public function load(ObjectManager $manager)
     {
+        $user = new User();
+        $user->setDisplayName("User1");
+        $user->setEmail("user1@snowtricks");
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'Password'));
+        $manager->persist($user);
+
+        $user = new User();
+        $user->setDisplayName("John Doe");
+        $user->setEmail("john@snowtricks");
+        $user->setPassword($this->passwordEncoder->encodePassword($user, 'Password'));
+        $user->addRole(USER::USER_VERIFIED);
+        $manager->persist($user);
+
+
+        $manager->flush();
 
     }
 
